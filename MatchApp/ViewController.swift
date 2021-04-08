4946eabc7fa6a4d082a8dd97446b700af5e43575
chenzhizs <chenzhi@qq.com>
2021-04-08 17:38:50 +0900

    https://www.youtube.com/watch?v=YQiJyNEI87I 第23课看完了，效果是出来了。但是我的图片大小与课程不一致。 可能是因为我的图片原来的大小问题。暂时没有找到应对的方法。 希望以后可以有办法修好。

--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource , UICollectionViewDelegate {
+    
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     let model = CardModel()
     var cardsArray = [Card]()
     
@@ -17,8 +20,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         cardsArray = model.getCards()
+        
+        //set the view controller an ther datesource and delegate of the collection view
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
+    
+    // MARK: - Collection View Delegate Methods
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // return number of cards
+        return cardsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        //Get a cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        //Configure it
+        
+        
+        //Return it
+        return cell
+    }
 
 }
 
